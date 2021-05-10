d010e478c0525e75c577d373eeae3ad66cf32797
leSamo <soleksak@redhat.com>
2021-05-11 00:28:03 +0200

    Doxygen improvement

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 #include "mainwindow.h"
 #include <QApplication>
 
+/*! \mainpage MQTT Explorer
+ * ICP project 2020/21
+ * Samuel Olekšák (xoleks00)
+ * Michal Findra (xfindr00)
+ *
+ * \section Description
+ * Application able to connect to MQTT broker and receive either
+ * all messages or messages of particular topic.
+ */
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     MainWindow w;
